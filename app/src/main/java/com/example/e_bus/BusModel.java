acf758f40e871e9e47a5bab42c59759618f5409e
fizza13 <fizzashoaib13@gmail.com>
2024-02-04 13:28:23 +0500

    1st update

---
+++ app/src/main/java/com/example/e_bus/BusModel.java
@@ -0,0 +1,16 @@
+package com.example.e_bus;
+
+
+// BusModel.java
+public class BusModel {
+    private String busNumber;
+
+    public BusModel(String busNumber) {
+        this.busNumber = busNumber;
+    }
+
+    public String getBusNumber() {
+        return busNumber;
+    }
+}
+
